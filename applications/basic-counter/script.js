1b7236e70f6c212c488f51c93d31861903109882
MarkLopresto <mark@spyderhaus.com>
2023-12-25 15:09:24 -0500

    update using basic operators
    `takeUntil`, `skipUntil`, and `scan`

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,18 +1,28 @@
 import { fromEvent, interval, timer, merge, NEVER } from 'rxjs';
+import { takeUntil, skipUntil, scan } from 'rxjs/operators';
 import { setCount, startButton, pauseButton, resetButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 
 let interval$ = interval(1000)
-let timer$ = timer(0, 1000)
+let timer$ = timer(0, 1000);
 let subscription;
 
-start$.subscribe(() => {
-  // subscription = interval$.subscribe(setCount);
-  subscription = timer$.subscribe(setCount);
-});
+// start$.subscribe(() => {
+//   // subscription = interval$.subscribe(setCount);
+//   subscription = timer$.subscribe(setCount);
+// });
 
-pause$.subscribe(() => {
-  subscription.unsubscribe();
-});
+// pause$.subscribe(() => {
+//   subscription.unsubscribe();
+// });
+
+
+const counter$ = interval$.pipe(
+  skipUntil(start$),
+  scan(count => count + 1, 0),
+  takeUntil(pause$),
+);
+
+counter$.subscribe(setCount);
