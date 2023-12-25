e5111835b08a0c3c0b559ce2a717a7b47a2d4a8c
MarkLopresto <mark@spyderhaus.com>
2023-12-25 13:56:53 -0500

    basic counter

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,5 +1,18 @@
-import { fromEvent, interval, merge, NEVER } from 'rxjs';
-import { setCount, startButton, pauseButton } from './utilities';
+import { fromEvent, interval, timer, merge, NEVER } from 'rxjs';
+import { setCount, startButton, pauseButton, resetButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+let interval$ = interval(1000)
+let timer$ = timer(0, 1000)
+let subscription;
+
+start$.subscribe(() => {
+  // subscription = interval$.subscribe(setCount);
+  subscription = timer$.subscribe(setCount);
+});
+
+pause$.subscribe(() => {
+  subscription.unsubscribe();
+});
