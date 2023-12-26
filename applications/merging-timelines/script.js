9848b22c1172a3c808687f546d8058e9acb45ec2
MarkLopresto <mark@spyderhaus.com>
2023-12-26 17:51:00 -0500

    mergin basics

--- applications/merging-timelines/script.js
+++ applications/merging-timelines/script.js
@@ -7,3 +7,25 @@ import {
   setStatus,
   bootstrap,
 } from './utilities';
+
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+
+const isRunning$ = merge(start$, pause$).pipe(startWith(false));
+
+isRunning$.subscribe(setStatus);
+
+const first$ = interval(1500).pipe(map(labelWith('First')), take(4));
+const second$ = interval(1000).pipe(map(labelWith('Second')), take(4));
+// const combined$ = merge(first$, second$);
+
+// * merge these two streams, but work on them in order
+// const combined$ = concat(first$, second$);
+
+// * change interval times to see the difference
+// const combined$ = race(first$, second$);
+
+// * deprecated: nothing in combined until both streams complete, then array of results
+const combined$ = forkJoin(first$, second$);
+
+bootstrap({ first$, second$, combined$ });
